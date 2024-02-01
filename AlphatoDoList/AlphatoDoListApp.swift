c4b1754168500cc46f514b4b4771cae25a806dc3
Mehmet Ali Er <mehmetalier83@hotmail.com>
2024-02-01 21:10:20 +0300

    Initial Commit

---
+++ AlphatoDoList/AlphatoDoListApp.swift
@@ -0,0 +1,17 @@
+// 
+//  AlphatoDoListApp.swift
+//  AlphatoDoList
+//
+//  Created by Mehmet Ali Er on 1.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlphatoDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
